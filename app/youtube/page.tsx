edef1e045dbd15b2b705e73c828108c4e96775ee
nyc1997 <nyc1997@gmail.com>
2026-08-26 09:50:12 +0900

    유튜브 iframe 추가

--- app/youtube/page.tsx
+++ app/youtube/page.tsx
@@ -17,6 +17,7 @@ export default function YouTubePage() {
           marginTop: '20px',
         }}
       >
+        {/*
         <iframe
           src="https://www.youtube.com/embed/Fprf4p1hFYw"
           title="YouTube video player"
@@ -28,6 +29,50 @@ export default function YouTubePage() {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         />
+
+        <br/>
+        */}
+        - T Square
+        <iframe
+          src="https://www.youtube.com/embed/RR1CEyHL7uE?si=wLlXXF1reWoyglld" 
+          title="YouTube video player"           
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',            
+          }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowfullscreen
+        />
+        
+        <br/>
+        - JPOP
+        <iframe
+          src="https://www.youtube.com/embed/B8RDImvf5DU?si=FWpjOzxojMAQDBKt" 
+          title="YouTube video player"           
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',            
+          }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowfullscreen
+        />
+        
+        <br/>
+        - Strings
+        <iframe
+          src="https://www.youtube.com/embed/xjYQlmpS69k?si=BEIsy92wpvHkHCvi" 
+          title="YouTube video player"           
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',            
+          }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowfullscreen
+        />
+
       </div>
     </main>
   )
